803058a1a576c776b94ef3aca8f57d3b851b107b
yamtimor <yamtimor@gmail.com>
2024-10-02 14:29:45 +0300

    adding the relevant parameters to executor class. later ill make the config file so that those parameters will be loaded from there.

--- src/executor.py
+++ src/executor.py
@@ -1,5 +1,7 @@
-
+import requests
 
 class Executor:
-    def __init__(self):
-        
+    def __init__(self, sql_query):
+        self.sql_query = sql_query
+        self.duckdb_conn = duckdb.connect(Config.DUCKDB_DATABASE_PATH)
+        self.es = Elasticsearch(Config.ELASTICSEARCH_HOST)
